722c466046cbb44b040704938faac9f60cf50712
dartsia <nichisniukd.7b@gmail.com>
2024-11-17 12:24:13 +0200

    Service for sending email

---
+++ services/sendEmail.js
@@ -0,0 +1,28 @@
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+const nodemailer = require("nodemailer");
+
+module.exports = async (sender, receiver, subject, text) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.HOST_EMAIL,
+            service: process.env.SERVICE_EMAIL,
+            port: Number(process.env.EMAIL_PORT),
+            secure: Boolean(process.env.SECURE),
+            auth: {
+                user: process.env.USER_EMAIL,
+                pass: process.env.PASS_EMAIL
+            }
+        });
+
+        await transporter.sendMail({
+            from: sender,
+            to: receiver,
+            subject: subject,
+            text: text
+        });
+        console.log("Email sent sucessfully.");
+    } catch (error) {
+        console.log("Email NOT sent." + error);
+    }
+}
